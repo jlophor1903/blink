98118d1cf6a11533cb73bf0040f93719f106013b
jlophor1903 <jlophor1903@g.educaand.es>
2025-03-20 12:30:29 +0100

    añadido environment esp01

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
-#define LED 13
 
 int contador = 0;
 
 void setup() {
- pinMode(LED, OUTPUT);
+ pinMode(LED_BUILTIN, OUTPUT);
  Serial.begin(9600);
 }
 
@@ -14,7 +13,7 @@ bool estado = 0;
 void loop() {
   int ahora = millis();
   if (ahora - antes > 1000) {
-  digitalWrite(LED, estado);
+  digitalWrite(LED_BUILTIN, estado);
   estado = !estado;
   antes = ahora;
 } 
